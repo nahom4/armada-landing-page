c4e81c0d2cbcce1ad227640672fe3f8b301d6480
Nahom Amare <nahomamare52@gmail.com>
2024-03-09 16:00:58 +0300

    changed padding

--- src/components/task_for_bussiness_component/Section1.tsx
+++ src/components/task_for_bussiness_component/Section1.tsx
@@ -4,10 +4,10 @@ import AdvertCard from "../../util/Card";
 import { IbmPlexSerif, nunito } from "@/app/fonts";
 function Section1(){
     return(
-        <div className="bg-blue-50 w-full flex flex-col items-center justify-center pt-5 md:pt-28 pb-14 border-b border-black">
+        <div className="bg-blue-50 w-full flex flex-col items-center justify-center pt-5 md:pt-28 pb-14 border-b border-black px-3">
             <div className="md:p-8 mt-8 flex flex-col items-center justify-center ">
                 <div className="p-8 flex justify-center flex-col items-center gap-6">
-                    <p className={`${nunito.className} md:text-2xl text-center font-semibold`}>
+                    <p className={`${nunito.className} md:text-2xl text-center`}>
                     Tasks for Business
                     </p>
                     <h1 className={`${IbmPlexSerif.className} text-[#222222] font-bold text-center`}>
